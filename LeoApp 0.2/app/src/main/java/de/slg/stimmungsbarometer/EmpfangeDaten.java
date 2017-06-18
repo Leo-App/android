e5f4a2d9111c1b333cb6fd5ada5cf6cea4ec0f96
moritz <moritz@liegmanns.de>
2017-06-18 21:12:41 +0200

    imports optimiert

--- LeoApp 0.2/app/src/main/java/de/slg/stimmungsbarometer/EmpfangeDaten.java
+++ LeoApp 0.2/app/src/main/java/de/slg/stimmungsbarometer/EmpfangeDaten.java
@@ -4,9 +4,7 @@ import android.os.AsyncTask;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.GregorianCalendar;
 
