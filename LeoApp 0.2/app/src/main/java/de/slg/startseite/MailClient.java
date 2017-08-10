7f47e16d98716027cbf32b4c476b002e54335457
LCA311 <spitzer-webdesign@outlook.de>
2017-08-10 14:00:48 +0200

    FeatureRequests möglich

---
+++ LeoApp 0.2/app/src/main/java/de/slg/startseite/MailClient.java
@@ -0,0 +1,73 @@
+package de.slg.startseite;
+
+import android.annotation.SuppressLint;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import de.slg.leoapp.List;
+
+class MailClient {
+    private final String emailPort = "587";
+    private final String smtpAuth = "true";
+    private final String starttls = "true";
+    private final String emailHost = "smtp.gmail.com";
+
+    private String fromEmail;
+    private String fromPassword;
+    private List<String> toEmailList;
+    private String emailSubject;
+    private String emailBody;
+
+    private Properties emailProperties;
+    private Session mailSession;
+    private MimeMessage emailMessage;
+
+    MailClient(String fromEmail, String fromPassword,
+                 List<String> toEmailList, String emailSubject, String emailBody) {
+        this.fromEmail = fromEmail;
+        this.fromPassword = fromPassword;
+        this.toEmailList = toEmailList;
+        this.emailSubject = emailSubject;
+        this.emailBody = emailBody;
+
+        emailProperties = System.getProperties();
+        emailProperties.put("mail.smtp.port", emailPort);
+        emailProperties.put("mail.smtp.auth", smtpAuth);
+        emailProperties.put("mail.smtp.starttls.enable", starttls);
+    }
+
+    MimeMessage createEmailMessage() throws
+            MessagingException, UnsupportedEncodingException {
+
+        mailSession = Session.getDefaultInstance(emailProperties, null);
+        emailMessage = new MimeMessage(mailSession);
+
+        emailMessage.setFrom(new InternetAddress(fromEmail, fromEmail));
+        for (String toEmail : toEmailList) {
+            emailMessage.addRecipient(Message.RecipientType.TO,
+                    new InternetAddress(toEmail));
+        }
+
+        emailMessage.setSubject(emailSubject);
+        emailMessage.setContent(emailBody, "text/html");
+        return emailMessage;
+    }
+
+    void sendEmail() throws MessagingException {
+        Transport transport = mailSession.getTransport("smtp");
+        transport.connect(emailHost, fromEmail, fromPassword);
+        transport.sendMessage(emailMessage, emailMessage.getAllRecipients());
+        transport.close();
+    }
+
+}
+
