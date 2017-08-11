b5814401c6e13b03fb1aeae5d28c560570101ace
moritz <moritz@liegmanns.de>
2017-08-11 21:34:06 +0200

    Lehrer sehen jetzt ihr Kürzel im NavigationHeader

--- LeoApp 0.2/app/src/main/java/de/slg/leoapp/PreferenceActivity.java
+++ LeoApp 0.2/app/src/main/java/de/slg/leoapp/PreferenceActivity.java
@@ -436,7 +436,10 @@ public class PreferenceActivity extends android.preference.PreferenceActivity im
         username.setText(Utils.getUserName());
 
         TextView grade = (TextView) navigationView.getHeaderView(0).findViewById(R.id.grade);
-        grade.setText(Utils.getUserStufe());
+        if (Utils.getUserPermission() == 2)
+            grade.setText(Utils.getLehrerKuerzel());
+        else
+            grade.setText(Utils.getUserStufe());
 
         ImageView mood = (ImageView) navigationView.getHeaderView(0).findViewById(R.id.profile_image);
         mood.setImageResource(Utils.getCurrentMoodRessource());
