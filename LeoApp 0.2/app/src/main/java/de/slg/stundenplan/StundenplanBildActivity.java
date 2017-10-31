d5ed45e7c1db8268ed8cd8101ad43932780b3735
luziBo <07lb2000@gmx.de>
2017-10-31 17:28:34 +0100

    Bitmap gemacht, speichern geht noch nicht wieder

--- LeoApp 0.2/app/src/main/java/de/slg/stundenplan/StundenplanBildActivity.java
+++ LeoApp 0.2/app/src/main/java/de/slg/stundenplan/StundenplanBildActivity.java
@@ -8,10 +8,11 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.widget.Toolbar;
+//import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -38,7 +39,7 @@ public class StundenplanBildActivity extends ActionLogActivity {
         setContentView(R.layout.activity_stundenplan_image);
         Utils.getController().registerStundenplanBildActivity(this);
         view = (StundenplanView) findViewById(R.id.image);
-        initToolbar();
+        //initToolbar();
     }
 
     @Override
@@ -74,7 +75,7 @@ public class StundenplanBildActivity extends ActionLogActivity {
         }
     }
 
-    private void initToolbar() {
+    /*private void initToolbar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitleTextColor(ContextCompat.getColor(getApplicationContext(), android.R.color.white));
         setSupportActionBar(toolbar);
@@ -82,9 +83,9 @@ public class StundenplanBildActivity extends ActionLogActivity {
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_arrow_back_white_24dp);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
-    }
+    }*/
 
-    private void saveImage() {
+    public void saveImage() {
         if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
         } else {
