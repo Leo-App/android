7234d775745abad689305bc8c4f00e17e209ec56
LCA311 <spitzer-webdesign@outlook.de>
2017-12-05 20:48:08 +0100

    ReceiveService Rework begonnen

--- LeoApp/app/src/main/java/de/slg/leoapp/service/ReceiveSyncAdapter.java
+++ LeoApp/app/src/main/java/de/slg/leoapp/service/ReceiveSyncAdapter.java
@@ -1,5 +1,12 @@
 package de.slg.leoapp.service;
 
+import android.accounts.Account;
+import android.content.AbstractThreadedSyncAdapter;
+import android.content.ContentProviderClient;
+import android.content.Context;
+import android.content.SyncResult;
+import android.os.Bundle;
+
 /**
  * ReceiveSyncAdapter.
  *
@@ -10,6 +17,18 @@ package de.slg.leoapp.service;
  * @version 2017.0512
  */
 
-public class ReceiveSyncAdapter {
-    //TODO
+public class ReceiveSyncAdapter extends AbstractThreadedSyncAdapter {
+
+    public ReceiveSyncAdapter(Context context, boolean autoInitialize) {
+        super(context, autoInitialize);
+    }
+
+    public ReceiveSyncAdapter(Context context, boolean autoInitialize, boolean allowParallelSyncs) {
+        super(context, autoInitialize, allowParallelSyncs);
+    }
+
+    @Override
+    public void onPerformSync(Account account, Bundle extras, String authority, ContentProviderClient provider, SyncResult syncResult) {
+
+    }
 }
---
+++ LeoApp/app/src/main/java/de/slg/leoapp/sync/NewsSynchronizer.java
@@ -0,0 +1,23 @@
+package de.slg.leoapp.sync;
+
+/**
+ * NewsSynchronizer.
+ *
+ * ...
+ *
+ * @author Gianni
+ * @since 0.6.8
+ * @version 2017.0512
+ */
+
+public class NewsSynchronizer implements SyncManager {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public boolean run() {
+        return false;
+    }
+}
