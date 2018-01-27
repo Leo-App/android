cd5b31277c45689d308fe30c177faf2dacce0067
luziBo <07lb2000@gmx.de>
2018-01-27 13:31:04 +0100

    Angefangen dialog

--- LeoApp/app/src/main/java/de/slg/stundenplan/dialog/CreateCourseDialog.java
+++ LeoApp/app/src/main/java/de/slg/stundenplan/dialog/CreateCourseDialog.java
@@ -6,10 +6,20 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatDialog;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import de.slg.leoapp.R;
 
 public class CreateCourseDialog extends AlertDialog {
+    EditText fachEingabe;
+    EditText lehrerEingabe;
+
+    Button cancel;
+    Button confirm;
+
+    //Den Rest bitte später
+
 
     public CreateCourseDialog(Context context) {
         super(context);
@@ -19,6 +29,46 @@ public class CreateCourseDialog extends AlertDialog {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dialog_custom_course);
+
+        fachEingabe = findViewById(R.id.course_name);
+        lehrerEingabe = findViewById(R.id.lehrer_edit);
+
+        cancel = findViewById(R.id.buttonDialog1);
+        confirm = findViewById(R.id.buttonDialog2);
+
+        //Den Rest später
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismiss();
+            }
+        });
+
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Datenbank und so ein Mist
+                //Und generell noch anderer Mist weil man nichts sieht aber nachdenken ist heute nicht so meine stärke
+                dismiss();
+            }
+        });
+
+        fachEingabe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fachEingabe = findViewById(R.id.course_name);
+            }
+        });
+
+        lehrerEingabe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                lehrerEingabe = findViewById(R.id.lehrer_edit);
+            }
+        });
+
     }
+
 
 }
