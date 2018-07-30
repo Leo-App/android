a4e4ecc0497ae3fe8be580b03d611d286fc18d58
MoritzLi <moritz@liegmanns.de>
2018-07-30 19:31:45 +0200

    Module + Abhängigkeiten erstellt

--- exams/src/test/java/de/leoapp_slg/exams/ExampleUnitTest.java
+++ exams/src/test/java/de/leoapp_slg/exams/ExampleUnitTest.java
@@ -1,8 +1,8 @@
-package de.leoapp_slg;
+package de.leoapp_slg.exams;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
--- pinboard/src/androidTest/java/de/leoapp_slg/pinboard/ExampleInstrumentedTest.java
+++ pinboard/src/androidTest/java/de/leoapp_slg/pinboard/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package de.leoapp_slg;
+package de.leoapp_slg.pinboard;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -7,7 +7,7 @@ import android.support.test.runner.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("de.leoapp_slg.test", appContext.getPackageName());
+        assertEquals("de.leoapp_slg.pinboard.test", appContext.getPackageName());
     }
 }
