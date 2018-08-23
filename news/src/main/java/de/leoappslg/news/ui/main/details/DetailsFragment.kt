57d45dfb7fbae4759c6f48751090d16516b9d266
lca311 <gspitzer@leoapp-slg.de>
2018-08-23 17:25:06 +0200

    Imports gefixt

--- news/src/main/java/de/leoappslg/news/ui/main/details/DetailsFragment.kt
+++ news/src/main/java/de/leoappslg/news/ui/main/details/DetailsFragment.kt
@@ -1,9 +1,18 @@
 package de.leoappslg.news.ui.main.details
 
 import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class DetailsFragment : Fragment(), IDetailsView {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
     override fun openDatePicker() {
         TODO("not implemented")
     }
@@ -12,7 +21,5 @@ class DetailsFragment : Fragment(), IDetailsView {
         TODO("not implemented")
     }
 
-    override fun getViewContext(): Context {
-        TODO("not implemented")
-    }
+    override fun getViewContext() = context!!
 }
--- news/src/main/java/de/leoappslg/news/utils/Converters.kt
+++ news/src/main/java/de/leoappslg/news/utils/Converters.kt
@@ -1,6 +1,6 @@
 package de.leoappslg.news.utils
 
-import android.arch.persistence.room.TypeConverter
+import androidx.room.TypeConverter
 import de.slg.leoapp.core.data.ProfilePicture
 import java.util.*
 
